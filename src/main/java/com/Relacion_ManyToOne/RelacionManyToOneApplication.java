4e2b35b0a24413696bbcb7298bd8ab7b8d14c748
Marcos <129058792+horingurky@users.noreply.github.com>
2025-05-16 10:41:35 +0200

    Initial commit

---
+++ src/main/java/com/Relacion_ManyToOne/RelacionManyToOneApplication.java
@@ -0,0 +1,13 @@
+package com.Relacion_ManyToOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelacionManyToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelacionManyToOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Relacion_ManyToOne/RelacionManyToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Relacion_ManyToOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionManyToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
